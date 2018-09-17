475d5592186a2bd00fd4d183ad602432ee243644
finn <finn>
2018-09-17 13:02:06 +0200

    update

--- js/plot-bivariate.js
+++ js/plot-bivariate.js
@@ -7,6 +7,7 @@ ftab["u1"] = 2*Math.PI;
 ftab["v0"] = 0;
 ftab["v1"] = Math.PI;
 ftab["d"] = set_distance;
+ftab["alpha"] = 0.94;
 
 var new_proj = new_proj_parallel;
 var proj_distance = 100;
@@ -174,7 +175,8 @@ function new_proj_perspective(phi,theta,px0,py0,mx){
         var yt = A[1][0]*x+A[1][1]*y+A[1][2]*z;
         var zt = A[2][0]*x+A[2][1]*y+A[2][2]*z;
         gxt=yt; gyt=0;
-        return [px0+mx*(-xt*r/(r-yt)),py0-mx*(zt*r/(r-yt))];
+        var a = yt<r?r/(r-yt):NaN;
+        return [px0+mx*(-xt*a),py0-mx*(zt*a)];
     };
 }
 
@@ -478,7 +480,8 @@ function plot(gx){
         }
     }
 
-    flush_tile_buffer(gx,240);
+    var alpha = Math.round(ftab["alpha"]*255);
+    flush_tile_buffer(gx,alpha);
     system_xyz(gx,10/ax);
 }
 
